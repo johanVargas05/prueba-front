a77136fa728db10ebfc7581c45670fea8153e6b8
Johan Vargas <johanvargas05.jv@gmail.com>
2021-03-03 05:10:35 -0500

    Maquetación checkout productos

---
+++ src/app/shared/productos-checkout/productos-checkout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-productos-checkout',
+  templateUrl: './productos-checkout.component.html',
+  styleUrls: ['./productos-checkout.component.scss']
+})
+export class ProductosCheckoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormularioComponent } from './formulario/formulario.component';
+import { ProductosCheckoutComponent } from './productos-checkout/productos-checkout.component';
 
 
 
 @NgModule({
-  declarations: [FormularioComponent],
+  declarations: [FormularioComponent, ProductosCheckoutComponent],
   imports: [
     CommonModule
   ],
-  exports:[FormularioComponent]
+  exports:[FormularioComponent,ProductosCheckoutComponent]
 })
 export class SharedModule { }
